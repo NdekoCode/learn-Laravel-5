a9909d86d214af1fc70c4698e18d73b931f4a328
Arick Bulakali <arickbulakali@gmail.com>
2022-04-12 11:06:08 +0200

    Dynamic link and other blade directives

--- resources/views/html_partials/navbar.blade.php
+++ resources/views/html_partials/navbar.blade.php
@@ -1,26 +1,48 @@
 <div class="container">
-    @include('html_partials.links',['classLink'=>'header__logo title title-2',
-    'link'=>'/','linkTitle'=>"Mon site",'list'=>false])
+    @include('html_partials.links', [
+        'classLink' => 'header__logo title title-2',
+        'link' => '/',
+        'linkTitle' => 'Mon site',
+        'list' => false,
+    ])
     <nav class="navbar">
         <ul class="navbar__list start">
-            
-    @include('html_partials.links',['classLink'=>'navbar__link',
-    'link'=>'/','linkTitle'=>"Acceuil",'list'=>true])
+
+            @include('html_partials.links', [
+                'classLink' => 'navbar__link',
+                'link' => '/',
+                'linkTitle' => 'Acceuil',
+                'list' => true,
+            ])
         </ul>
         <ul class="navbar__list end">
-            @if (auth()->guest())
-            
-    @include('html_partials.links',['classLink'=>'btn navbar__link',
-    'link'=>'/login','linkTitle'=>"Connexion",'list'=>true])
-    
-    @include('html_partials.links',['classLink'=>'btn navbar__link',
-    'link'=>'/signin','linkTitle'=>"Inscription",'list'=>true])
+            {{-- On verifie si l'utilisateur est connecté --}}
+            @auth
+
+                @include('html_partials.links', [
+                    'classLink' => 'btn navbar__link',
+                    'link' => '/logout',
+                    'linkTitle' => 'Deconnexion',
+                    'list' => true,
+                ])
+                <li class="list__item"><a class=" navbar__link-img {{ request()->is('profile') ? 'is-active' : '' }}"
+                        href="/profile"><img class="user-link" src="{{ asset('img/user.svg') }}" width="40"
+                            height="40" alt=""></a></li>
             @else
-            
-    @include('html_partials.links',['classLink'=>'btn navbar__link',
-    'link'=>'/logout','linkTitle'=>"Deconnexion",'list'=>true])
-                <li class="list__item"><a class=" navbar__link-img {{ request()->is('profile') ? 'is-active' :''  }}" href="/profile"><img class="user-link" src="{{ asset('img/user.svg') }}" width="40" height="40" alt=""></a></li>
-            @endif
+                @include('html_partials.links', [
+                    'classLink' => 'btn navbar__link',
+                    'link' => '/login',
+                    'linkTitle' => 'Connexion',
+                    'list' => true,
+                ])
+
+                @include('html_partials.links', [
+                    'classLink' => 'btn navbar__link',
+                    'link' => '/signin',
+                    'linkTitle' => 'Inscription',
+                    'list' => true,
+                ])
+            @endauth
         </ul>
     </nav>
 </div>
