6875d11ca1ffe684e0fa541183574b92140fd84c
Arick Bulakali <arickbulakali@gmail.com>
2022-04-17 19:20:22 +0200

    Sort by Desc and Laravel Array order Proxy

--- app/Http/Controllers/newsController.php
+++ app/Http/Controllers/newsController.php
@@ -11,17 +11,12 @@ class newsController extends Controller
         // La methode flattent() nous permet d'eviter d'avoir des tableaux dans des tableaux ou des tableaux avec des dispositions complexe
         /** On recupere la relation et ainsi les personnes que suis l'utilisateur
          * Puis on execute une fonction de transformation qui transforme les utilisateurs suivis en leurs messages
-         * On execute une foncton qui eviter les imbrications complexes des tableaux et nous renvois juste un tableau d'un niveau simple
-         * On trie nos messages obtenus selon leurs date de publication et en ayant vers le plus recent
+         * On execute une foncton qui eviter les imbrications complexes des tableaux et nous renvois juste un tableau d'un niveau
          * 
-         * map->message  et sortByDesc->created_at:Laravel Array Order Proxy c'est pas du php, c'est du laravel, imaginer que cela equivaut à: 
-         *  map(function ($user) { return $user->messages; }) et sortByDesc(function (message) { message->created_at})
          */
-        $messages = auth()->user()->follow->map->messages->flatten()->sortByDesc->created_at;
-        // On peut remplacer encore ce code par
-        /**
-         * auth()-user()->follow->flapMap->message->orderByDesc->created_at
-         */
+        $messages = auth()->user()->follow->map(function ($user) {
+            return $user->messages;
+        })->flatten();
 
         return view('news',[
             'messages' => $messages
--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -3,6 +3,29 @@
 @section('content')
     <div>
         <h2 class="title title-2">Actualités</h2>
+        @auth
+            <h4 class="title title-4">Entrer votre message</h4>
+            <form action="/messages" method="post">
+                @csrf
+
+                <div class="form-container h-full w-full">
+                    @if ($errors->has('auth_errors'))
+                        <div class="alert alert-danger">{{ $errors->first('auth_errors') }}</div>
+                    @endif
+                    <div class="mb-3">
+                        <textarea class="px-2 py-2 border rounded w-full focus:ring focus:ring-offset-2 transition-all outline-none {{ $errors->has('message') ? 'border-red-500' : '' }}"
+                            name="message" id="message" placeholder="Qu'avez-vous à dire aujourd'hui ?"></textarea>
+                        @if ($errors->has('message'))
+                            <p class="font-thin text-red-500 mt-1 text-sm">
+
+                                {{ $errors->first('message') }}</p>
+                        @endif
+                    </div>
+                    <button class="border px-2 py-2 rounded bg-blue-700 text-white transition-all hover:bg-blue-800"
+                        type="submit">Publier</button>
+
+            </form>
+        @endauth
 
 
         <ul class="mt-5">
