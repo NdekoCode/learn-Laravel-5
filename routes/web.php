60010e9a4860819087e9689f446c62f65008d6af
Arick Bulakali <arickbulakali@gmail.com>
2022-04-17 19:55:43 +0200

    Laravel Debugbar

--- routes/web.php
+++ routes/web.php
@@ -17,43 +17,40 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 // On simplifie ce qu'il y a au dessus
-Route::view('/home','welcome');
-Route::view('/about','about');
+Route::view('/home', 'welcome');
+Route::view('/about', 'about');
 
-Route::view('/hello/{firstname}','hello',['firstname'=>request('firstname')]);
+Route::view('/hello/{firstname}', 'hello', ['firstname' => request('firstname')]);
 
 Route::get('/signin', 'SigninController@formSignin');
 
 Route::post('/signin', 'SigninController@signinTraitement');
 
-Route::get('/login','LoginController@formLogin');
+Route::get('/login', 'LoginController@formLogin');
 
-Route::post('/login','LoginController@loginTraitement');
+Route::post('/login', 'LoginController@loginTraitement');
 
 
 Route::get('/', 'UsersController@usersList');
 
-Route::get('/logout','AccountController@logout');
+Route::get('/logout', 'AccountController@logout');
 
 // Un groupe de route dependant d'un middleware
 Route::group([
     // Le middleware à appelé pour tous ce groupe de route
-    'middleware'=> 'App\Http\Middleware\Auth'
+    'middleware' => 'App\Http\Middleware\Auth'
 ], function () {
     // Un groupe de route impacter par le middleware `Auth`
     // A chaque qu'un utilisateurs voudra acceder à l'une de ces routes, ce middleware sera appelé
-Route::get('/profile','AccountController@profile'); 
-Route::post('/updated-password','AccountController@updatePassword');
+    Route::get('/profile', 'AccountController@profile');
+    Route::post('/updated-password', 'AccountController@updatePassword');
 
-Route::post('/messages', 'MessagesController@new');
+    Route::post('/messages', 'MessagesController@new');
 
-Route::get('/news','NewsController@list');
+    Route::get('/news', 'NewsController@list');
 
-Route::post('/follow/{email}','FollowController@new');
-Route::delete('/follow/{email}','FollowController@remove');
-
-
+    Route::post('/follow/{email}', 'FollowController@new');
+    Route::delete('/follow/{email}', 'FollowController@remove');
 });
 
-
-Route::get('/{email}','UsersController@seeUser');
+Route::get('/{email}', 'UsersController@seeUser');
